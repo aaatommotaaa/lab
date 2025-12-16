e8eac9c31963a21d9fd0038455043977b6f51dcf
Atom <chanidapa.atom@gmail.com>
2025-12-16 14:35:49 +0700

    name

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+  // switch(rank){
+  //   case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
+  //   case 'A': cout << "You have received 5 gems.\n";
+  //   case 'B': cout << "You have received 1 gems.\n";
+  //   case 'C': cout << "You have received 2000 coins.\n";
+  //   case 'D': cout << "You have received very KAK items.\n";
+  // }
